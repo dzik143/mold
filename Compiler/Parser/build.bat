ca8345151310a1dfee0abb26b43f86f0897c44c1
Sylwester Wysocki <sw143@wp.pl>
2020-07-18 10:04:58 +0200

    Compiler/Parser: Added JS postprocess.

--- Compiler/Parser/build.bat
+++ Compiler/Parser/build.bat
@@ -1,5 +1,2 @@
-echo Generating LALR(1) parser code...
-call unicc -l MoldParser -b _Parser Mold.par
-
-echo Generating LALR(1) parser tables...
-call unicc -l MoldTables -b _Tables Mold.par
+call unicc mold.par
+node _BuildTables.js >_Tables.mold
