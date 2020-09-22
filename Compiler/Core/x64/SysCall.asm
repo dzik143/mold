987ee56d17819782a2795a1ee9d51f0e5996dc16
Sylwester Wysocki <sw143@wp.pl>
2020-09-22 04:38:18 +0200

    Compiler/X64: Simplify passing return value for syscall.

--- Compiler/Core/x64/SysCall.asm
+++ Compiler/Core/x64/SysCall.asm
@@ -28,21 +28,22 @@ __MOLD_SysCall:
 
 ; ------------------------------------------------------------------------------
 ; Open file
-; rcx - path
+; rcx - path (IN)
+; rdx - file handle (OUT)
 ; ------------------------------------------------------------------------------
 
 .openForRead:
-    mov     edx, GENERIC_READ
+    mov     r8d, GENERIC_READ
     mov     eax, OPEN_EXISTING
     jmp     .openFinal
 
 .openForWrite:
-    mov     edx, GENERIC_READ or GENERIC_WRITE
+    mov     r8d, GENERIC_READ or GENERIC_WRITE
     mov     eax, OPEN_ALWAYS
     jmp     .openFinal
 
 .openForCreate:
-    mov     edx, GENERIC_READ or GENERIC_WRITE
+    mov     r8d, GENERIC_READ or GENERIC_WRITE
     mov     eax, CREATE_ALWAYS
 
 .openFinal:
@@ -50,10 +51,12 @@ __MOLD_SysCall:
     mov     rcx, [rcx + Buffer_t.bytesPtr]
     lea     rcx, [rcx + String_t.text]
 
-    cinvoke CreateFileA, rcx, rdx, FILE_SHARE_READ, 0, rax, 0, 0
+    push    rdx
+    cinvoke CreateFileA, rcx, r8, FILE_SHARE_READ, 0, rax, 0, 0
+    pop     rdx
 
-    mov     [rdi + Variant_t.value], rax
-    mov     [rdi + Variant_t.type], VARIANT_INTEGER
+    mov     [rdx + Variant_t.value], rax
+    mov     [rdx + Variant_t.type], VARIANT_INTEGER
     ret
 
 ; ------------------------------------------------------------------------------
@@ -68,12 +71,14 @@ __MOLD_SysCall:
 
 ; ------------------------------------------------------------------------------
 ; Read from file
+; rcx - file handle (IN)
+; rdx - read value  (OUT)
 ; ------------------------------------------------------------------------------
 
 .read_char:
     mov     r8, 1
     mov     eax, VARIANT_STRING
-    mov     [rdi + Variant_t.flags], VARIANT_FLAG_ONE_CHARACTER
+    or      [rdx + Variant_t.flags], VARIANT_FLAG_ONE_CHARACTER
     jmp     .read_final
 
 .read_int8:
@@ -127,10 +132,11 @@ __MOLD_SysCall:
 
 .read_final:
     sub     rsp, 32
-    mov     [rdi + Variant_t.type], eax
-    mov     [rdi + Variant_t.value], 0
+    mov     [rdx + Variant_t.type], eax
+    mov     [rdx + Variant_t.value], 0
+
     mov     rcx, [rcx + Variant_t.value] ; hFile
-    lea     rdx, [rdi + Variant_t.value] ; lpBuffer
+    lea     rdx, [rdx + Variant_t.value] ; lpBuffer
     mov     r9, NumberOfBytesReaded      ; lpNumberOfBytesRead
     mov     qword [rsp], 0               ; lpOverlapped
 
@@ -141,6 +147,8 @@ __MOLD_SysCall:
 
 ; ------------------------------------------------------------------------------
 ; Write file
+; rcx - file handle    (IN)
+; rdx - value to write (IN)
 ; ------------------------------------------------------------------------------
 
 .writeSizeTable:
@@ -167,58 +175,90 @@ __MOLD_SysCall:
     ret
 
 ; ------------------------------------------------------------------------------
+; Load file content to the string
+; rcx - file path (IN)
+; rdx - file content (OUT)
+; ------------------------------------------------------------------------------
+
+.loadFile:
+    ; TODO: Clean up this mess.
+    push    rdi
+    mov     rdi, rdx
+    call    __MOLD_LoadFile
+    pop     rdi
+    ret
+
+; ------------------------------------------------------------------------------
 ; Convert byte to char
+; rcx - ascii code (IN)
+; rdx - one character string (OUT)
 ; ------------------------------------------------------------------------------
 
 .asc:
     mov     rax, [rcx + Variant_t.value]
-    mov     [rdi + Variant_t.type], VARIANT_STRING
-    mov     [rdi + Variant_t.flags], VARIANT_FLAG_ONE_CHARACTER
-    mov     [rdi + Variant_t.value], rax
+    mov     [rdx + Variant_t.type], VARIANT_STRING
+    mov     [rdx + Variant_t.flags], VARIANT_FLAG_ONE_CHARACTER
+    mov     [rdx + Variant_t.value], rax
     ret
 
 ; ------------------------------------------------------------------------------
 ; Convert char to byte
+; rcx - one character string (IN)
+; rdx - ascii code (OUT)
 ; ------------------------------------------------------------------------------
 
 .ord:
     mov     rax, [rcx + Variant_t.value]
-    mov     [rdi + Variant_t.type], VARIANT_INTEGER
-    mov     [rdi + Variant_t.value], rax
+    mov     [rdx + Variant_t.type], VARIANT_INTEGER
+    mov     [rdx + Variant_t.value], rax
     ret
 
 ; ------------------------------------------------------------------------------
 ; Convert string to integer
+; rcx - number as text e.g. '1234' (IN)
+; rdx - integer value (OUT)
 ; ------------------------------------------------------------------------------
 
 .parse_integer:
+    push    rdx                                      ;
+
     mov     rcx, [ rcx + Variant_t.value ]           ; rcx = text (Buffer_t)
     mov     rcx, [ rcx + Buffer_t.bytesPtr ]         ; rcx = text (String_t)
     lea     rcx, [ rcx + String_t.text ]             ; rcx = text (char*)
     mov     rdx, 0                                   ; rdx = temp buf = NULL
     mov     r8, rdx                                  ; rdx = base = 0 = auto
+
     cinvoke strtol                                   ; rcx = int(text)
 
-    mov     [rdi + Variant_t.type], VARIANT_INTEGER
-    mov     [rdi + Variant_t.value], rax
-    ret
+    pop     rdx                                      ;
+    mov     [rdx + Variant_t.type], VARIANT_INTEGER  ;
+    mov     [rdx + Variant_t.value], rax             ;
+    ret                                              ;
 
 ; ------------------------------------------------------------------------------
 ; Convert string to float
+; rcx - number as text e.g. '3.14' (IN)
+; rdx - float value (OUT)
 ; ------------------------------------------------------------------------------
 
 .parse_float:
     mov     rcx, [rcx + Variant_t.value]             ; rcx = text (Buffer_t)
     mov     rcx, [rcx + Buffer_t.bytesPtr]           ; rcx = text (String_t)
     lea     rcx, [rcx + String_t.text]               ; rcx = text (char*)
-    cinvoke atof                                     ; rcx = atof(text)
 
-    mov     [rdi + Variant_t.type], VARIANT_DOUBLE
-    movq    [rdi + Variant_t.value], xmm0
-    ret
+    push    rdx                                      ;
+    cinvoke atof                                     ; rcx = atof(text)
+    pop     rdx                                      ;
+
+    mov     [rdx + Variant_t.type], VARIANT_DOUBLE   ;
+    movq    [rdx + Variant_t.value], xmm0            ;
+    ret                                              ;
 
 ; ------------------------------------------------------------------------------
-; Bitwise operations
+; Bitwise operations: rv = x op y
+; rcx - x  (IN)
+; rdx - y  (IN)
+; r8  - rv (OUT)
 ; ------------------------------------------------------------------------------
 
 .bitwise_and:
@@ -241,8 +281,8 @@ __MOLD_SysCall:
     not rax                           ; rax = ~x
 
 .bitwise_done:
-    mov [rdi + Variant_t.type], VARIANT_INTEGER
-    mov [rdi + Variant_t.value], rax
+    mov [r8 + Variant_t.type], VARIANT_INTEGER
+    mov [r8 + Variant_t.value], rax
     ret
 
 ; ------------------------------------------------------------------------------
@@ -296,24 +336,18 @@ __MOLD_SysCall:
     jmp     __MOLD_Halt
 
 .str:
-    ; TODO: Change function proto and call directly.
-    mov     rdx, rdi
     jmp     __MOLD_VariantConvertToString
 
 .len:
-    ; TODO: Change function proto and call directly.
-    mov     rdx, rdi
     jmp     __MOLD_VariantLength
 
 .typeof:
-    ; TODO: Change function proto and call directly.
-    mov     rdx, rdi
     jmp     __MOLD_VariantTypeOf
 
 .getTypeId:
     mov     eax, [rcx + Variant_t.type]
-    mov     [rdi + Variant_t.type], VARIANT_INTEGER
-    mov     [rdi + Variant_t.value], rax
+    mov     [rdx + Variant_t.type], VARIANT_INTEGER
+    mov     [rdx + Variant_t.value], rax
     ret
 
 .innertypeof:
@@ -327,7 +361,6 @@ __MOLD_SysCall:
     mov     eax, [rax + Array_t.innerType]  ; eax = x.innerType
     mov     [rcx + Variant_t.type], eax     ; tmp.type = x.innerType
 
-    mov     rdx, rdi
     jmp     __MOLD_VariantTypeOf
 
 ; ------------------------------------------------------------------------------
@@ -359,10 +392,21 @@ __MOLD_SysCall:
     ; TODO
     jmp     .error
 
-; TODO: Clean up this mess.
-; TODO: Optimize it.
-; TODO: Validate args.
+; ------------------------------------------------------------------------------
+; rv = substr(text, startIdx, length)
+;
+; rcx - text      (IN)
+; rdx - start idx (IN)
+; r8  - length    (IN)
+; r9  - rv        (OUT)
+; ------------------------------------------------------------------------------
+
 .substr:
+
+    ; TODO: Clean up this mess.
+    ; TODO: Optimize it.
+    ; TODO: Validate args.
+
     ; Fetch args
     cmp    [rcx + Variant_t.type], VARIANT_STRING
     jnz    __MOLD_PrintErrorAndDie.stringExpected
@@ -386,47 +430,49 @@ __MOLD_SysCall:
     ja      __MOLD_PrintErrorAndDie.indexOutOfRange
 
     ; Calculate maximum possible length
-    mov     r9, [rcx + String_t.length]    ;
-    sub     r9, rdx                        ; r9 = maximum possible length
+    mov     rax, [rcx + String_t.length]   ; rax = text.length (int32)
+    sub     rax, rdx                       ; rax = maximum possible length
 
     ; Limit length to maximum possible
-    cmp     r9, r8
-    cmovbe  r8, r9
+    cmp     rax, r8
+    cmovbe  r8, rax
 
     ; -1: Use maximum possible length
     or      r8, r8                         ;
-    cmovs   r8, r9                         ;
+    cmovs   r8, rax                        ;
 
 .init_output_stub:
     push    rcx
     push    rdx
     push    r8
+    push    r9
 
     lea     rcx, [r8 + 16]                         ; rcx      = len + 16
-
     call    __MOLD_MemoryAlloc                     ; rax      = new Buffer_t
-    mov     [rdi + Variant_t.type], VARIANT_STRING ; rv.type  = string
-    mov     [rdi + Variant_t.flags], 0             ; rv.flags = 0
-    mov     [rdi + Variant_t.value], rax           ; rv.value = new Buffer_t
 
+    pop     r9
     pop     r8
     pop     rdx
     pop     rcx
 
-    ; Fill up output data
-    push    rdi
+    mov     [r9 + Variant_t.type], VARIANT_STRING  ; rv.type  = string
+    mov     [r9 + Variant_t.flags], 0              ; rv.flags = 0
+    mov     [r9 + Variant_t.value], rax            ; rv.value = new Buffer_t
 
-    mov     rdi, [rax + Buffer_t.bytesPtr]         ; rdi       = new String_t
-    mov     [rdi + String_t.length], r8            ; rv.length = len
+    ; Fill up output data
+    push    r9
+
+    mov     r9 , [rax + Buffer_t.bytesPtr]         ; r9        = new String_t
+    mov     [r9  + String_t.length], r8            ; rv.length = len
 
     lea     rdx, [rcx + String_t.text + rdx]
-    lea     rcx, [rdi + String_t.text]
+    lea     rcx, [r9  + String_t.text]
 
     cinvoke strncpy
 
-    pop     rdi
+    pop     r9
 
-    DEBUG_CHECK_VARIANT rdi
+    DEBUG_CHECK_VARIANT r9
 
     ret
 
@@ -475,7 +521,7 @@ __MOLD_SysCall:
   dq .error         ; 26
   dq .error         ; 27
   dq .error         ; 28
-  dq __MOLD_LoadFile; 29
+  dq .loadFile      ; 29
   dq .error         ; 30
 
   dq .ord           ; 31
@@ -491,12 +537,13 @@ __MOLD_SysCall:
 
   dq __MOLD_Halt    ; 40
   dq .die           ; 41
-  dq .str           ; 42 ; TODO: __MOLD_VariantConvertToString
-  dq .len           ; 43 ; TODO: __MOLD_VariantLength
-  dq .typeof        ; 44 ; TODO: __MOLD_VariantTypeOf
 
+  dq __MOLD_VariantConvertToString ; 42
+  dq __MOLD_VariantLength          ; 43
+  dq __MOLD_VariantTypeOf          ; 44
   dq __MOLD_PrintVariant           ; 45
   dq __MOLD_PrintVariantToStdError ; 46
+
   dq .error                        ; 47
   dq .error                        ; 48
   dq .error                        ; 49
