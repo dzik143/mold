d8d6259ddd14e0f88b2969c20202b53c5384f89f
Sylwester Wysocki <dzik@ke.mu>
2023-09-03 14:52:46 +0200

    Core/X64: Fixed error message while type mismatch in VariantCastToXxx().

--- Compiler/Core/x64/variant/VariantCastTo.asm
+++ Compiler/Core/x64/variant/VariantCastTo.asm
@@ -20,13 +20,13 @@ __MOLD_VariantCastTo_int64:
 __MOLD_VariantCastTo_bool32:
 
     cmp    [rcx + Variant_t.type], VARIANT_BOOLEAN ; Is it boolean?
-    jnz    __MOLD_PrintErrorAndDie.integerExpected ; Fail if non-boolean
+    jnz    __MOLD_PrintErrorAndDie.booleanExpected ; Fail if non-boolean
     mov    ecx, dword [rcx + Variant_t.value]      ; rcx = x.value
     ret                                            ;
 
 __MOLD_VariantCastTo_float64:
 
     cmp    [rcx + Variant_t.type], VARIANT_DOUBLE  ; Is it float64?
-    jnz    __MOLD_PrintErrorAndDie.integerExpected ; Fail if non-float64
+    jnz    __MOLD_PrintErrorAndDie.floatExpected   ; Fail if non-float64
     movq   xmm0, qword [rcx + Variant_t.value]     ; rcx = x.value
     ret                                            ;
